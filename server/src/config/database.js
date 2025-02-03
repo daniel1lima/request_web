248228fa6a1ff91875eb5fec5c68e4cd913ac1e7
Daniel Lima <damorosolima@gmail.com>
2025-02-03 13:59:35 -0800

    Revert "fix for dotenv"
    This reverts commit dc095123cd70e6635a17ce9ad623423d48ac72aa.

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.PGDATABASE,
-  process.env.PGUSER,
-  process.env.PGPASSWORD,
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
   }
 );
--- server/src/routes/stripe.js
+++ server/src/routes/stripe.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-require('dotenv').config();
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY; // Use environment variable for the secret key
 if (!stripeSecretKey) {
   throw new Error("Stripe secret key is not defined in environment variables");
