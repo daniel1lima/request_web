dc095123cd70e6635a17ce9ad623423d48ac72aa
Daniel Lima <damorosolima@gmail.com>
2025-02-03 13:13:26 -0800

    fix for dotenv

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
   {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
     dialect: 'postgres',
   }
 );
