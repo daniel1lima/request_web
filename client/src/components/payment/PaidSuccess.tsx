59ebc55133a808db5002a70cf51ab1983872c257
Daniel Lima <dlima@students.cs.ubc.ca>
2025-03-14 21:45:25 -0700

    Remove text from paid success page

--- client/src/components/payment/PaidSuccess.tsx
+++ client/src/components/payment/PaidSuccess.tsx
@@ -39,10 +39,6 @@ const PaidSuccess: React.FC = () => {
             Your song will not show up on the song queue{" "}
             <strong>until the DJ accepts</strong> the request.
           </li>
-          <li className="mb-2 text-center ">
-            <strong>We reserve the right to deny your request</strong> if you
-            approach the DJ.
-          </li>
           <li className="mb-2 text-center text-red-400">
             To <strong>cancel your request</strong> check your text message
             confirmation
